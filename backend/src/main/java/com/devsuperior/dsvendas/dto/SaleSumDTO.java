8584c865d11fc85a985e694579fa6610483672b9
Vinícius Lima <vinicius.vailant@gmail.com>
2021-05-08 14:33:29 -0400

    Group by seller success

---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleSumDTO.java
@@ -0,0 +1,48 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+/**
+ * Esta classe cria uma consulta de resultados totais
+ * de vendas de cada vendedor.
+ * @author vinic
+ *
+ */
+public class SaleSumDTO implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	private String sellerName;
+	private Double sum;
+	
+	public SaleSumDTO() {}
+
+	public SaleSumDTO(Seller seller, Double sum) {
+		this.sellerName = seller.getName();
+		this.sum = sum;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public Double getSum() {
+		return sum;
+	}
+
+	public void setSum(Double sum) {
+		this.sum = sum;
+	}
+	
+}
--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsvendas.repositories;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsvendas.entities.Seller;
